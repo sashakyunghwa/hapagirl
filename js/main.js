4fec675a3df19832f9732914935dd729e7a61462
Sasha <sashakyunghwa@gmail.com>
2018-09-14 16:36:44 -0700

    account-layout

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
 
+// Shop Page
 var shirtIcons = document.getElementsByClassName('shirt-icons');
+var versionOneTee = document.getElementById('version-one-tee');
+var versionTwoTee = document.getElementById('version-two-tee');
+var versionThreeTee = document.getElementById('version-three-tee');
  
 // Add event listeners
 for(i = 0; i < shirtIcons.length; i++){
@@ -9,14 +13,16 @@ for(i = 0; i < shirtIcons.length; i++){
 
 });
 
-
 function shirtIconReplacesShirtImage(){
     console.log('shirt icon clicked');
     var iconSource = this.src;
     var newIconSource = this.parentElement.parentElement.children[0].children[0].src;
     this.parentElement.parentElement.children[0].children[0].src = iconSource;
     this.src = newIconSource;
-    // $('.image-row-one').css('visibility', 'hidden');
-    // $('.version-two-tee').css('visibility', 'visible');
-    // $('.version-three-tee').css('visibility', 'visible');
 }
+
+function checkWhichVersionTeePage(){
+    if()
+}
+
+versionOneTee.addEventListener('click', );
