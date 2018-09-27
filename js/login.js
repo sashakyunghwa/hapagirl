1cae1bf807b218060842cb970d59c4e589f42b3d
Sasha <sashakyunghwa@gmail.com>
2018-09-27 02:04:21 -0700

    wishlist and cart linked up to pages

--- js/login.js
+++ js/login.js
@@ -3,6 +3,8 @@ $(document).ready(function() {
 // Declare variables
 var loginButton = document.getElementById('login-button');
 var registerButton = document.getElementById('register-button');
+var loginEmail = document.getElementById('login-email').value;
+var loginPassword = document.getElementById('login-password').value;
     
 // Add event listeners   
 loginButton.addEventListener('click', checkIfLoginMatchesObjPeople);
@@ -11,30 +13,48 @@ registerButton.addEventListener('click', addNewUserToObjPeople);
 // Add functions    
 function checkIfLoginMatchesObjPeople(){
     for(i = 0; i < objPeople.length; i++) {
-        if(email == objPeople[i].email && password == objPeople[i].password) {
+        if(loginEmail == objPeople[i].email && loginPassword == objPeople[i].password) {
             console.log(email + ' is logged in!');
             return
+        } else if(loginEmail.length < 7 || loginEmail.length === '') {
+            $('.student-icon').popover('show');
+            emailValid = false;
+            console.log('incorrect username or password');
+        } else {
+            $('.student-icon').popover('hide');
+            emailValid = true;
         }
-    }
-    console.log('incorrect username or password');
+    }   
 }   
 
 });
 // Document.ready ends
 
+var emailValid = null;
 var objPeople = [];
         
 function addNewUserToObjPeople(){
     // for(i = 0; i < objPeople.length; i++) {
         // objPeople[i].email.push(email) && objPeople[i].password.push(password);
-        var email = document.getElementById('email-register').value;
-        var password = document.getElementById('password-register').value;
-        objPeople.push({
-            'email': email,
-            'password': password
+        var registerEmail = document.getElementById('register-email').value;
+        var registerPassword = document.getElementById('register-password').value;
+     
+        if(registerEmail.length < 7 || registerEmail.length === '') {
+            $('.student-icon').popover('show');
+            emailValid = false;
+        } else {
+            $('.student-icon').popover('hide');
+            emailValid = true;
+        }
+
+        if(emailValid) {
+            objPeople.push({
+            'email': registerEmail,
+            'password': registerPassword
         });
-        console.dir(document.getElementById('email-register'));
+        console.dir(document.getElementById('register-email'));
         console.log(objPeople);
+        }
 }
 
 
